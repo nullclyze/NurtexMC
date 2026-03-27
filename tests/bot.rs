a4e5b41e513eb336e914c31200164e8e0ada4dfd
nullclyze <fliget45d1mw@gmail.com>
2026-03-27 16:29:25 +0400

    Update tests

--- tests/bot.rs
+++ tests/bot.rs
@@ -2,35 +2,29 @@
 mod tests {
   use std::io;
 
-  use nurtex::core::bot::{AutoReconnectPlugin, AutoRespawnPlugin, Bot, BotCommand, BotPlugins};
+  use nurtex::HumanoidArm;
+  use nurtex::common::client_information::ClientInformation;
+  use nurtex::core::bot::{AutoReconnectPlugin, Bot, BotPlugins};
   use nurtex::core::events::BotEvent;
   use nurtex::packets::game::ClientboundGamePacket;
-  use nurtex::utils::sleep;
-  use uuid::Uuid;
 
   #[tokio::test]
   async fn launch_bot() -> io::Result<()> {
-    let (mut bot, terminal) = Bot::new("NurtexBot", Uuid::nil());
+    let (mut bot, _terminal) = Bot::new("NurtexBot");
 
     bot = bot
       .set_event_listener(event_listener)
+      .set_information(ClientInformation {
+        main_hand: HumanoidArm::Left,
+        ..Default::default()
+      })
       .set_plugins(BotPlugins {
         auto_reconnect: AutoReconnectPlugin {
           enabled: false,
           reconnect_delay: 0
         },
-        auto_respawn: AutoRespawnPlugin {
-          enabled: false,
-        },
         ..Default::default()
       });
-
-    tokio::spawn(async move {
-      sleep(3000).await;
-      terminal.send(BotCommand::Chat("Test".to_string())).await;
-      sleep(5000).await; 
-      terminal.send(BotCommand::Disconnect).await;
-    });
 
     bot.connect_to("localhost", 25565).await?;
 
@@ -48,6 +42,12 @@ mod tests {
       BotEvent::Chat { sender_uuid, message } => {
         if let Some(uuid) = sender_uuid {
           println!("Bot {} received a message from {}: {}", bot.username, uuid, message);
+        }
+
+        if message.contains("entities") {
+          for (id, entity) in &bot.storage.entities {
+            println!("{} - {:?}", id, entity);
+          }
         }
       }
       BotEvent::Packet(packet) => match packet {
--- tests/swarm.rs
+++ tests/swarm.rs
@@ -4,26 +4,28 @@ mod tests {
 
   use nurtex::{create_shared_swarm, launch_shared_swarm};
   use nurtex::core::bot::BotCommand;
-  use nurtex::core::swarm::BotConfig;
+  use nurtex::core::swarm::SwarmObject;
   use nurtex::utils::sleep;
 
   #[tokio::test]
   async fn launch_swarm() -> io::Result<()> {
-    let mut configs = Vec::new();
+    let mut objects = Vec::new();
 
-    for i in 0..4 {
-      let config = BotConfig::new(format!("bot_{}", i));
-      configs.push(config);
+    for i in 0..=50 {
+      let object = SwarmObject::new(format!("bot_{}", i));
+      objects.push(object);
     }
 
-    let swarm = create_shared_swarm(configs);
+    let swarm = create_shared_swarm(objects);
 
-    launch_shared_swarm(swarm.clone(), "localhost".to_string(), 25565, 500);
+    launch_shared_swarm(swarm.clone(), "localhost".to_string(), 25565, 25);
 
-    sleep(4000).await;
+    sleep(5000).await;
     swarm.read().await.send(BotCommand::Chat("Test".to_string())).await;
-    sleep(5000).await; 
-    swarm.read().await.send(BotCommand::Disconnect).await;
+
+    sleep(5000).await;
+
+    swarm.write().await.destroy().await;
 
     Ok(())
   }
